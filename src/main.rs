396b20fba835933c2e8ddb3398a9bfed4be87740
BrainVader <brainvade@gmail.com>
2020-05-08 23:27:06 +0900

    Add Deserialize trait for Human struct

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-#[derive(Debug)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
 enum Episode {
     Jo,
     Ha,
     Q,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 struct Human {
     id: String,
     name: String,
@@ -18,5 +20,17 @@ fn main() {
         name: "Ikari Shinji".to_owned(),
         appears_in: vec![Episode::Jo, Episode::Ha, Episode::Q],
     };
-    println!("{:?}", ikari_shinji);
+    let ikari_shinji_str =
+        serde_json::to_string(&ikari_shinji).expect("cant't convert into string");
+
+    println!("result \n {}", ikari_shinji_str);
+
+    let ayanami_rei_str = "
+    {
+        \"id\": \"2\",
+        \"name\": \"Ayanami Rei\",
+        \"appears_in\": [\"/episode/Jo\", \"/episode/Ha\", \"/episode/Q\"]
+    }";
+    let anayami_rei: Human = serde_json::from_str(&ayanami_rei_str).unwrap();
+    println!("{:?}", anayami_rei);
 }
