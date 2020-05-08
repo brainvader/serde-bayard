c013dc6bd5296cb06682e5a9bd9a616eca832d77
BrainVader <brainvade@gmail.com>
2020-05-08 22:53:25 +0900

    Add Human struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+enum Episode {
+    Jo,
+    Ha,
+    Q,
+}
+
+struct Human {
+    id: String,
+    name: String,
+    appears_in: Vec<Episode>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let ikari_shinji = Human {
+        id: "1".to_owned(),
+        name: "Ikari Shinji".to_owned(),
+        appears_in: vec![Episode::Jo, Episode::Ha, Episode::Q],
+    };
 }
