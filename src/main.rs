27dc5d0c34f2c8deb68b6977c82b3a8f20276af0
BrainVader <brainvade@gmail.com>
2020-05-10 13:01:22 +0900

    Make  json string deserializable with custom function

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,57 @@
-use serde::{Deserialize, Serialize};
+use serde::{de, Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 enum Episode {
+    #[serde(alias = "/episode/Jo")]
     Jo,
+    #[serde(alias = "/episode/Ha")]
     Ha,
+    #[serde(alias = "/episode/Q")]
     Q,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Human {
+    #[serde(deserialize_with = "deserialize_from_array")]
+    #[serde(alias = "_id")]
     id: String,
+    #[serde(deserialize_with = "deserialize_from_array")]
     name: String,
     appears_in: Vec<Episode>,
+}
+
+fn deserialize_from_array<'de, T, D>(deserializer: D) -> Result<T, D::Error>
+where
+    T: Deserialize<'de>,
+    D: de::Deserializer<'de>,
+{
+    struct ActualDataVisitor<T>(std::marker::PhantomData<fn() -> T>);
+
+    impl<'de, T> de::Visitor<'de> for ActualDataVisitor<T>
+    where
+        T: Deserialize<'de>,
+    {
+        // Deserialize into
+        type Value = T;
+
+        // For error message
+        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+            formatter.write_str("a nonempty sequence of data")
+        }
+
+        fn visit_seq<S>(self, mut seq: S) -> Result<T, S::Error>
+        where
+            S: de::SeqAccess<'de>,
+        {
+            // You can use IgnoredAny to skip over the first nth elements.
+            let actual_data = seq
+                .next_element()?
+                .ok_or_else(|| de::Error::custom("no values in seq when looking for maximum"))?;
+            Ok(actual_data)
+        }
+    }
+    let visitor = ActualDataVisitor(std::marker::PhantomData);
+    deserializer.deserialize_seq(visitor)
 }
 
 fn main() {
@@ -23,14 +63,9 @@ fn main() {
     let ikari_shinji_str =
         serde_json::to_string(&ikari_shinji).expect("cant't convert into string");
 
-    println!("result \n {}", ikari_shinji_str);
+    println!("Ikari Shinji \n {}", ikari_shinji_str);
 
-    let ayanami_rei_str = "
-    {
-        \"id\": \"2\",
-        \"name\": \"Ayanami Rei\",
-        \"appears_in\": [\"/episode/Jo\", \"/episode/Ha\", \"/episode/Q\"]
-    }";
+    let ayanami_rei_str = "{\"_id\":[\"1\"],\"appears_in\":[\"/episode/Jo\",\"/episode/Ha\",\"/episode/Q\"],\"name\":[\"Ikari Shinji\"]}";
     let anayami_rei: Human = serde_json::from_str(&ayanami_rei_str).unwrap();
     println!("{:?}", anayami_rei);
 }
